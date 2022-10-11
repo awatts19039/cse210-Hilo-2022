6740f5f02e0be1c6a06aba257f7a7b22390e5252
Andrew Watts <awatts@byui.edu>
2022-10-11 15:13:16 -0600

    Score

--- Director.cs
+++ Director.cs
@@ -12,7 +12,8 @@ namespace Unit03.Game
         bool _isPlaying = true;
         int _cardOne = 0;
         int _cardTwo = 0;
-
+        int _score = 0;
+        int _totalScore = 300;
         public void StartGame()
         {
             while (_isPlaying)
@@ -30,16 +31,35 @@ namespace Unit03.Game
             Console.Write($"The card is: {_cardOne}");
             Console.Write("Higher or lower? [h/l] ");
             string _userChoice = Console.ReadLine();
+           _isPlaying = (_userChoice == "h");
         }
         public void DoUpdates()
         {
             if (_cardOne > _cardTwo && _userChoice == "l") {
                 _answercorrect = true;
+                _totalScore = _score + 100;
+
+            }
+            else if (_cardOne < _cardTwo && _userChoice == "h"){
+                _answercorrect = true;
+                _totalScore = _score + 100;
+            }
+            else{
+                _answercorrect = false;
+                _totalScore = _score -75;
             }
         }
         public void DoOutputs()
         {
-            
+            if (!_isPlaying)
+            {
+                return;
         }
+
+        
+
+            Console.WriteLine($"The second card is: {_cardTwo}");
+            Console.WriteLine($"Your score is: {_totalScore}\n");
+            _isPlaying = (_score > 0 );
     }
 }
